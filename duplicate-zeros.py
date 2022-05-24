5dceb4661d07d968253d4b2f46883892e1d69569
rpaut03l <66171949+rpaut03l@users.noreply.github.com>
2022-05-24 19:09:45 +0800

    duplicate each occu

---
+++ duplicate-zeros.py
@@ -0,0 +1,70 @@
+# Duplicate Zeros #
+Q. Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+Example 1:
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+Example 2:
+Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+
+Hint: https://leetcode.com/problems/duplicate-zeros/discuss/898225/Python-2-solutions-Easy-to-understand-Time-O(N)-Space-O(1)
+
+Solution 1: 
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        len_ = len(arr)
+        i = 0
+        while( i < len_):
+            if arr[i] == 0:
+                arr.insert(i, 0) # i=index, 0=item
+                arr.pop() # remove the last element of the array and also returns the removed element. This function decreases the len of array, # by 1.
+                i += 2
+                continue
+            else:
+                i += 1
+
+
+Solution 2: covering edge cases
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+
+        possible_dups = 0
+        length_ = len(arr) - 1
+
+        # Find the number of zeros to be duplicated
+        for left in range(length_ + 1):
+
+            # Stop when left points beyond the last element in the original list
+            # which would be part of the modified list
+            if left > length_ - possible_dups:
+                break
+
+            # Count the zeros
+            if arr[left] == 0:
+                # Edge case: This zero can't be duplicated. We have no more space,
+                # as left is pointing to the last element which could be included  
+                if left == length_ - possible_dups:
+                    arr[length_] = 0 # For this zero we just copy it without duplication.
+                    length_ -= 1
+                    break
+                possible_dups += 1
+
+        # Start backwards from the last element which would be part of new list.
+        last = length_ - possible_dups
+
+        # Copy zero twice, and non zero once.
+        for i in range(last, -1, -1):
+            if arr[i] == 0:
+                arr[i + possible_dups] = 0
+                possible_dups -= 1
+                arr[i + possible_dups] = 0
+            else:
+                arr[i + possible_dups] = arr[i]
+        
